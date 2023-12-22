c08b8afa9ded843dedad28cc79f9019497efef31
floralgreen <carducci.federico20@gmail.com>
2023-12-22 17:16:38 +0100

    Esercizio Completato

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Punto punto1 = new Punto(-50, 80);
+
+        System.out.println("Cordinate punto 1 " + punto1);
+
+    }
+
+}
